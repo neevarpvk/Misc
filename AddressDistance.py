48380a83ae5c7e1ac63bb110a43434be4f9d5c22
neevarpvk <63980879+neevarpvk@users.noreply.github.com>
2020-06-06 10:47:30 -0500

    Update AddressDistance.py

--- AddressDistance.py
+++ AddressDistance.py
@@ -1,31 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun  4 17:03:40 2020
-
-@author: praveenvudumu
-"""
-
-from geopy.geocoders import Nominatim
-geolocator = Nominatim()
-
-from1 = geolocator.geocode("8510 e 29th st n, wichita")
-fromcor = ((from1.latitude, from1.longitude))
-
-to = geolocator.geocode("4111 e 37th st n, wichita")
-tocor = ((to.latitude, to.longitude))
-
-from geopy.distance import geodesic
-print(geodesic(fromcor, tocor).miles)
-
-from geopy.distance import great_circle
-print(great_circle(fromcor, tocor).miles)
-
-#df[full_add] = df["st"]+", "+df["st2"]+", "+df["city"]
-
-###########################################################################
-
 import pandas as pd
+import numpy as np
 df = pd.read_csv('/Users/praveenvudumu/downloads/goepy_address.csv')
 from geopy.geocoders import Nominatim
 geolocator = Nominatim()
@@ -47,8 +21,9 @@ from geopy.distance import geodesic
 def distance(row):
     add1 = (row['lat1'], row['lon1'])
     add2 = (row['lat2'], row['lon2'])
-    return (geodesic(add1, add2).miles)
-
-#df['dist_btw'] = df.apply(lambda row: distance(row), axis = 1 )
+    try:
+        return (geodesic(add1, add2).miles)
+    except ValueError:
+        return np.nan
 
 df['dist_btw'] = df.apply(lambda row: distance(row), axis = 1 )
